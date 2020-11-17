d86e94ae41710aeabd833983fdf002b56493f18b
Maddula Sai <maddula.sai@ad.infosys.com>
2020-11-17 23:15:30 +0530

    added comment for trigger

--- src/SimpleAPI/Startup.cs
+++ src/SimpleAPI/Startup.cs
@@ -29,6 +29,7 @@ namespace SimpleAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //Comment to check auto trigger
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
