fb53a39fa48888265d0ad92c4644697172743193
Maddula Sai <maddula.sai@ad.infosys.com>
2020-11-18 15:29:34 +0530

    Changed path to CTC

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace SimpleAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("ctc")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
